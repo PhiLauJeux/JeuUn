bd00f5a3ebc3162300cbfc5ef674a43941f0309e
Phil <philippe.thibeault99@outlook.com>
2018-12-12 23:53:35 -0500

    Parallax

---
+++ 2D_Platformer/Library/Assets/Scripts/DeplacementBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementBackground : MonoBehaviour {
+
+    public float vitesse;
+
+    public Vector3 posXSpawn;
+    public Vector3 posXMax;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position - (Vector3.left * (vitesse * Time.deltaTime));
+        if (transform.position.x <= posXMax.x)
+        {
+            transform.position = posXSpawn;
+        }
+	}
+}
